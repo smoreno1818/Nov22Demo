72a500d7a715ba3c74a6aba056ef7945213ebe23
smoreno1818 <117882847+smoreno1818@users.noreply.github.com>
2022-11-09 19:14:05 -0800

    Add files via upload

---
+++ Module 2 practice video prob sheet.py
@@ -0,0 +1,19 @@
+x = 5;
+y = 10;
+x = y; # The variable x is set to the same value as 
+print(x);
+
+# can run without semi colons
+# always adds up to whatever is on the left
+
+intFirstNumber = 5
+print(type(intFirstNumber))
+
+intFirstNumber = 5.8
+print(type(intFirstNumber))
+
+intFirstNumber = "5"
+print(type(intFirstNumber))
+
+intFirstNumber = True
+print(type(intFirstNumber))
